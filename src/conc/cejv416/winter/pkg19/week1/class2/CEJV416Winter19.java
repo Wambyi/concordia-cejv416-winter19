48b4b7657cc335fce53d3fbfba98f6be7186be63
gl_dani <gl_dani@CCE-FB107-10W.concordia.ca>
2019-01-18 19:33:36 -0500

    Initial commit

---
+++ src/conc/cejv416/winter/pkg19/week1/class2/CEJV416Winter19.java
@@ -0,0 +1,23 @@
+package conc.cejv416.winter.pkg19.week1.class2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author gl_dani
+ */
+public class CEJV416Winter19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
