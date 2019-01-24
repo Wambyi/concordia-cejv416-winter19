ae2343f79484b3f56dd0dbec8bb1464ecb24531d
gl_dani <gl_dani@CCE-FB107-10W.concordia.ca>
2019-01-23 21:22:33 -0500

    Added 'age' and 'variables'

---
+++ src/conc/cejv416/winter/pkg19/week2/class1/Task1/Adder.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conc.cejv416.winter.pkg19.week2.class1.Task1;
+
+/**
+ *
+ * @author gl_dani
+ */
+public class Adder {  
+    
+    //default values assigned to the variables
+    int num1 = 0;  
+    int num2 = 0;
+    
+    //create a method called 'add'
+    public void add()  
+    {
+        System.out.println("The values added are " + (num1 + num2));
+    }
+    
+}
