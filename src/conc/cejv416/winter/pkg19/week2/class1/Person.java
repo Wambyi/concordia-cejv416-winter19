c59b7e0721a3ef2c631d83aaf9c353c76dfc8d65
gl_dani <gl_dani@CCE-FB107-10W.concordia.ca>
2019-01-23 21:22:44 -0500

    Added 'age' and 'variables'

---
+++ src/conc/cejv416/winter/pkg19/week2/class1/Person.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conc.cejv416.winter.pkg19.week2.class1;
+
+/**
+ * This is a new class called 'Person'
+ * e.g See specification document A.23.
+ * @author gl_dani
+ */
+public class Person { 
+    
+    String name = "Unknown ";  //default name
+    int age = 0; //default age = 0
+    
+    // Method 
+    public void sayAge()  
+    {
+        System.out.println("Hello, my name is " + name + " and age is " + age);
+        //Scanner Ask =  new Scanner (System.in);
+    }
+        
+}
